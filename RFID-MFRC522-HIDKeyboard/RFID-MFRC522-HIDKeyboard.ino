6d1eecfdadbcb70f40cbed498fd23551c0bc39e1
ixd-hof <m@tsuM1.fritz.box>
2024-01-12 16:58:41 +0100

    Code cleaned up

--- RFID-MFRC522-HIDKeyboard/RFID-MFRC522-HIDKeyboard.ino
+++ RFID-MFRC522-HIDKeyboard/RFID-MFRC522-HIDKeyboard.ino
@@ -1,25 +1,4 @@
 /*
-   --------------------------------------------------------------------------------------------------------------------
-   Example sketch/program showing how to read data from a PICC to serial.
-   --------------------------------------------------------------------------------------------------------------------
-   This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
-
-   Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
-   Reader on the Arduino SPI interface.
-
-   When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
-   then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
-   you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
-   will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
-   when removing the PICC from reading distance too early.
-
-   If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
-   So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
-   details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
-   keep the PICCs at reading distance until complete.
-
-   @license Released into the public domain.
-
    Typical pin layout used:
    -------------------------------------------------------------------------------------------------------
                MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino     Raspberry Pico
@@ -33,7 +12,8 @@
    SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15          GP18
 */
 
-/* Libraries needed:
+/*
+Libraries needed:
 - USBHID by Various
 - MFRC522 by GithubCommunity
 */
@@ -71,10 +51,8 @@ long last_code = 0;
 long card = 0;
 
 void setup() {
-  /*Serial.begin(9600);  // Initialize serial communications with the PC
-  while (!Serial)
-    ;                                 // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-    */
+  Serial.begin(9600);  // Initialize serial communications with the PC
+
   SPI.begin();                        // Init SPI bus
   mfrc522.PCD_Init();                 // Init MFRC522
   delay(4);                           // Optional delay. Some board do need more time after init to be ready, see Readme
@@ -86,13 +64,8 @@ void loop() {
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   card = 0;
 
-  if (card == 1) {
-    //Serial.print("Card: ");
-  }
-
   if (mfrc522.PICC_IsNewCardPresent()) {
     card = 1;
-    //Serial.println("New Card: ");
   } else {
     if (mfrc522.PICC_IsNewCardPresent())
       card = 1;
@@ -100,7 +73,6 @@ void loop() {
       if (card == 1)
         Keyboard.printf("x");
       card = 0;
-      //Serial.println("No Card: ");
     }
   }
 
@@ -113,41 +85,18 @@ void loop() {
     for (byte i = 0; i < mfrc522.uid.size; i++) {
       code = ((code + mfrc522.uid.uidByte[i]) * 10);  // Nun werden wie auch vorher die vier Blöcke ausgelesen und in jedem Durchlauf wird der Code mit dem Faktor 10 „gestreckt“. (Eigentlich müsste man hier den Wert 1000 verwenden, jedoch würde die Zahl dann zu groß werden.
     }
-    /*Serial.print("Code: ");
-    Serial.println(code);-
-    Serial.print("Last Code: ");
-    Serial.println(last_code);*/
+    Serial.println(code);
 
-
-
-    // Ahsen
-
+    // if code then key
     if (code == 736490) {
       Keyboard.printf("A");
     } else if (code == 1273520) {
-      //Keyboard.key_code(222, 0);//printf("B");
       Keyboard.printf("B");
-      Keyboard.printf(0);
     } else if (code == 2128480) {
       Keyboard.printf("C");
     } else if (code == 2164270) {
       Keyboard.printf("D");
-    }
-
-    /*
-    if (code == 736490) {
-      Keyboard.printf("J");
-    } else if (code == 1273520) {
-      Keyboard.printf("K");
-    } else if (code == 2128480) {
-      Keyboard.printf("L");
-    } else if (code == 2164270) {
-      Keyboard.printf("M");
-    }
-  */
-
-  // Friederike
-    if (code == 553570) {
+    } else if (code == 553570) {
       Keyboard.printf("E");
     } else if (code == 2186790) {
       Keyboard.printf("F");
@@ -157,22 +106,12 @@ void loop() {
       Keyboard.printf("H");
     }
 
-
-
     mfrc522.PICC_HaltA();
 
-    //byte bufferATQA[2];
-    //byte bufferSize = sizeof(bufferATQA);
-    //mfrc522.PICC_WakeupA(bufferATQA, &bufferSize);
-
-    // Dump debug info about the card; PICC_HaltA() is automatically called
-    //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
     last_code = code;
   } else if (card == 0) {
     //Serial.print("No Card: ");
   }
 
   delay(500);
-
-  //Keyboard.printf("Hello");
 }
